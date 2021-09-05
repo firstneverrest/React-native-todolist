f08d8163af09ca1bee03cb4a86da2d886a678765
Chitsanupong <first30952@gmail.com>
2021-09-05 23:12:18 +0700

    feat: change header and add logout

--- todolist/src/routes/Routes.tsx
+++ todolist/src/routes/Routes.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MaterialIcons } from '@expo/vector-icons';
 
 // screens
 import LoginScreen from '../screens/LoginScreen';
 import TodoListScreen from '../screens/TodoListScreen';
+import { logout } from '../utils';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +14,29 @@ const Routes: React.FC = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="TodoList" component={TodoListScreen} />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{
+            headerTitle: 'Todo List Application',
+          }}
+        />
+        <Stack.Screen
+          name="TodoList"
+          component={TodoListScreen}
+          options={({ navigation }) => ({
+            headerTitle: 'Todo List Application',
+            headerLeft: (props) => <></>,
+            headerRight: (props) => (
+              <MaterialIcons
+                name="logout"
+                size={24}
+                color="black"
+                onPress={() => logout(navigation)}
+              />
+            ),
+          })}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
