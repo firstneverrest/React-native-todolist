6dc5e8a4fed8dd46dcd0079f4e775126273ecbb5
Chitsanupong <first30952@gmail.com>
2021-09-05 23:13:52 +0700

    feat: change logout to use navigation

--- todolist/src/utils/logout.ts
+++ todolist/src/utils/logout.ts
@@ -1,5 +1,15 @@
 import { deleteData } from './storage';
+import { Alert } from 'react-native';
 
-export const logout = () => {
-  deleteData('token');
+export const logout = (navigation: any) => {
+  Alert.alert('ออกจากระบบ', 'ท่านต้องการออกจากระบบหรือไม่', [
+    {
+      text: 'ออกจากระบบ',
+      onPress: () => {
+        deleteData('token');
+        navigation.navigate('Login');
+      },
+    },
+    { text: 'ยกเลิก', onPress: () => null },
+  ]);
 };
