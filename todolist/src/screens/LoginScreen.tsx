6314366fcc0090bbe6c8e1214185eedabc1201c0
Chitsanupong <first30952@gmail.com>
2021-09-05 15:38:36 +0700

    feat: add axios and authentication

--- todolist/src/screens/LoginScreen.tsx
+++ todolist/src/screens/LoginScreen.tsx
@@ -6,17 +6,52 @@ import {
   TouchableWithoutFeedback,
   Keyboard,
 } from 'react-native';
-import { Button, Title, TextInput } from 'react-native-paper';
+import { Button, TextInput } from 'react-native-paper';
 import { NavigationProps } from 'type.model';
 import globalStyles from '../styles/global';
+import axios from 'axios';
+import { Login } from '../type.model';
 
 // icon
 import { AntDesign } from '@expo/vector-icons';
 import colors from '../styles/colors';
 
+// store token
+import { storeData } from '../utils';
+
 const LoginScreen = ({ navigation }: NavigationProps) => {
+  const [message, setMessage] = useState('');
+  const [isError, setIsError] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+
+  const loginHandler = () => {
+    const data = {
+      userid: username,
+      password: password,
+    };
+
+    axios
+      .post<Login>('https://learningportal.ocsc.go.th/todoapi/tokens', data)
+      .then((response) => {
+        if (response.status === 200 || 201) {
+          const token = response.data.token;
+          storeData('token', token);
+          setMessage('');
+          setIsError(false);
+          setUsername('');
+          setPassword('');
+          console.log(token);
+
+          navigation.navigate('TodoList');
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsError(true);
+        setMessage('ชื่อผู้ใช้หรือรหัสผ่านไม่ถูกต้อง');
+      });
+  };
 
   return (
     <TouchableWithoutFeedback
@@ -27,7 +62,7 @@ const LoginScreen = ({ navigation }: NavigationProps) => {
       <View style={globalStyles.container}>
         <View style={styles.loginContainer}>
           <AntDesign name="profile" size={36} color="black" />
-          <Title style={globalStyles.header}>เข้าสู่ระบบ</Title>
+          <Text style={globalStyles.header}>เข้าสู่ระบบ</Text>
         </View>
         <TextInput
           style={styles.inputField}
@@ -44,13 +79,12 @@ const LoginScreen = ({ navigation }: NavigationProps) => {
           value={password}
           onChangeText={(password) => setPassword(password)}
         />
+        {isError && <Text style={styles.errorText}>{message}</Text>}
         <Button
           style={styles.submitButton}
           icon="login"
           mode="contained"
-          onPress={() => {
-            navigation.navigate('TodoList');
-          }}
+          onPress={() => loginHandler()}
         >
           เข้าสู่ระบบ
         </Button>
@@ -78,6 +112,11 @@ const styles = StyleSheet.create({
     width: '80%',
     marginTop: 6,
     backgroundColor: colors.darkPurple,
+  },
+  errorText: {
+    color: 'red',
+    paddingVertical: 6,
+    alignSelf: 'center',
   },
 });
 
