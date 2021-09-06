13ea3db9c8929bc0431021d839233df7719bdeec
Chitsanupong <first30952@gmail.com>
2021-09-06 14:23:18 +0700

    feat: add modal style

--- todolist/src/styles/global.ts
+++ todolist/src/styles/global.ts
@@ -1,45 +1,82 @@
 import { StyleSheet } from 'react-native';
-import color from './colors';
+import colors from './colors';
 
 const globalStyles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    marginTop: 16,
+    paddingTop: 16,
   },
   header: {
     fontFamily: 'sarabun-bold',
     fontSize: 20,
     marginVertical: 4,
     lineHeight: 40,
+    alignSelf: 'center',
   },
   body: {
     fontFamily: 'sarabun-regular',
     fontSize: 16,
   },
   primary: {
-    color: color.darkPurple,
+    color: colors.darkPurple,
   },
   secondary: {
-    color: color.lightPurple,
+    color: colors.lightPurple,
   },
   lightGrey: {
-    color: color.lightGrey,
+    color: colors.lightGrey,
   },
   darkGrey: {
-    color: color.darkGrey,
+    color: colors.darkGrey,
   },
   white: {
-    color: color.white,
+    color: colors.white,
   },
   black: {
-    color: color.black,
+    color: colors.black,
   },
   error: {
-    color: color.default.error,
+    color: colors.default.error,
   },
   success: {
-    color: color.default.success,
+    color: colors.default.success,
+  },
+  modalContainer: {
+    backgroundColor: 'white',
+    padding: 20,
+    marginHorizontal: 16,
+    zIndex: 10,
+  },
+  inputField: {
+    height: 50,
+    fontFamily: 'sarabun-regular',
+    paddingHorizontal: 12,
+    fontSize: 16,
+    borderColor: colors.darkPurple,
+    borderWidth: 2,
+    borderRadius: 4,
+    width: '100%',
+    marginBottom: 10,
+  },
+  submitButton: {
+    alignSelf: 'center',
+    height: 40,
+    fontSize: 12,
+    width: 100,
+    marginTop: 26,
+    backgroundColor: colors.darkPurple,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    fontSize: 16,
+  },
+  errorText: {
+    color: colors.default.error,
+    fontSize: 16,
   },
 });
 
